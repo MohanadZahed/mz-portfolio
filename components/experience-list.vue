b3eb4c910ac59daf2f35fbc6fc44d6cb0722d556
Mohanad Zahed <mzahed@medienwerft.de>
2024-04-09 09:31:44 +0200

    changing light header color to green

--- components/experience-list.vue
+++ components/experience-list.vue
@@ -1,7 +1,7 @@
 <template>
   <h3 class="mb-5 text-primary-800">{{ $t('experience') }}</h3>
   <ol class="group/list">
-    <li v-for="experience in experiences" :key="experience.title" class="mb-12">
+    <li v-for="experience in experiences" :key="experience.title" class="mb-12 last:mb-0">
       <experience-element :experience="experience" />
     </li>
   </ol>
