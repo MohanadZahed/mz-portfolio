39429fc508f2e716d4d9b674586057a8df0dc5cc
Mohanad Zahed <mzahed@medienwerft.de>
2024-04-23 14:29:05 +0200

    change page transition and update CVs

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style>
-.page-enter-active,
+/*.page-enter-active,
 .page-leave-active {
   transition: all 0.2s;
 }
@@ -13,5 +13,31 @@
 .page-leave-to {
   opacity: 0.5;
   filter: grayscale(1);
+}*/
+
+.page-enter-active,
+.page-leave-active {
+  transition: all .2s cubic-bezier(0.75, 0.25, 0, 1.05);
+}
+.page-enter-from {
+  transform: translateX(-40px);
+  opacity: 0
+}
+
+.page-leave-to {
+  filter: blur(1rem);
+  transform: translateX(40px);
+  opacity: 0;
+}
+
+@media (max-width: 767px) {
+  .page-enter-from {
+    transform: translateY(15px);
+  }
+
+  .page-leave-to {
+    filter: blur(0.5rem);
+    transform: translateY(15px);
+  }
 }
 </style>
